6d26ff90892eaadf64627aeddcb2de2cc8e10fb1
Shamee Mahmud <sami_safin_mahmud@hotmail.com>
2019-06-22 01:40:45 -0500

    adjusted node.js to fit the same configuration

--- nodejs/index.js
+++ nodejs/index.js
@@ -17,7 +17,7 @@ var options = {
 };
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
+var spec = fs.readFileSync(path.join(__dirname,'api/openapi.yaml'), 'utf8');
 var swaggerDoc = jsyaml.safeLoad(spec);
 
 // Initialize the Swagger middleware
